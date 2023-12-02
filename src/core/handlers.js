0ec49865719b8430ce3e5fc01774a9e9fb87d0ef
Okkar25 <okkaaung2508@gmail.com>
2023-12-02 08:29:36 +0630

    third commit

--- src/core/handlers.js
+++ src/core/handlers.js
@@ -1,5 +1,5 @@
 import { url } from "./functions";
-import { createCourseForm, drawerRightExample } from "./selectors";
+import { createCourseForm } from "./selectors";
 
 export const createCourseFormHandler = async (event) => {
   event.preventDefault();
@@ -12,6 +12,12 @@ export const createCourseFormHandler = async (event) => {
     fee: formData.get("course_fees"),
   };
 
+  // disable form (to avoid "CREATE" btn being clicked multiple times)
+  createCourseForm
+    .querySelectorAll("input")
+    .forEach((el) => el.toggleAttribute("disabled"));
+  createCourseForm.querySelector("button").toggleAttribute("disabled");
+
   const response = await fetch(url("/courses"), {
     method: "POST",
     headers: { "content-type": "application/json" },
@@ -21,5 +27,10 @@ export const createCourseFormHandler = async (event) => {
   console.log(data);
 
   createCourseForm.reset(); // clearing out data
-  drawerRightExample.close();
+
+  // undo disable form
+  createCourseForm
+    .querySelectorAll("input")
+    .forEach((el) => el.toggleAttribute("disabled"));
+  createCourseForm.querySelector("button").toggleAttribute("disabled");
 };
--- src/core/selectors.js
+++ src/core/selectors.js
@@ -1,2 +1,1 @@
 export const createCourseForm = document.querySelector("#createCourseForm");
-
